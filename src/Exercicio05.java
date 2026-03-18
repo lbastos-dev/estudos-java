909065f305fac10fbab08057cc2eea695ee166c7
lbastos-dev <bastos.dev@edu.unifor.br>
2026-03-18 09:28:20 -0300

    Conclusão do Exercício 05

---
+++ src/Exercicio05.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio05 {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int funcionario = sc.nextInt();
+        int horasTrabalhadas = sc.nextInt();
+        double valorHora = sc.nextDouble();
+
+
+        double salario = horasTrabalhadas * valorHora;
+        System.out.printf("FUNCIONARIO = %1d%n" +
+                "SALARIO = %.2f%n",funcionario,salario);
+
+
+
+    }
+}
