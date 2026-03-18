176eb3fe3b508fe4679e425bccc652d1fe2b7d42
lbastos-dev <bastos.dev@edu.unifor.br>
2026-03-18 11:02:12 -0300

    Criação de pastas p/ separação de exercícios. Início de estruturas condicionais e Exercício 01 de estruturas condicionais

---
+++ src/EstruturaCondicional/Exercicio01.java
@@ -0,0 +1,18 @@
+package EstruturaCondicional;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        if (num < 0 ){
+            System.out.println("NEGATIVO");
+        }else {
+            System.out.println("NÃO NEGATIVO");
+        }
+    }
+}
--- src/EstruturaSequencial/Exercicio05.java
+++ src/EstruturaSequencial/Exercicio05.java
@@ -1,3 +1,5 @@
+package EstruturaSequencial;
+
 import java.util.Locale;
 import java.util.Scanner;
 
